38226541dbf7de7a13fd58b47b5de1f1fbdd7949
Byoung-Wha Cho <whqudghk28@gmail.com>
2024-02-22 20:20:59 +0900

    yes

---
+++ ex00/whatever.hpp
@@ -0,0 +1,26 @@
+#ifndef eyy_HPP
+#define eyy_HPP
+
+template <typename T>
+void swap(T& a, T& b) {
+  T tmp;
+  tmp = a;
+  a = b;
+  b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+  if (a > b) return b;
+  else return a;
+}
+
+template <typename T>
+T max(T a , T b)
+{
+  if (a > b) return a;
+  else return b;
+}
+
+#endif
